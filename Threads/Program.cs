d3259bc31e90b8a321b96dbb9f8e1e89a435132a
Deivid Higino Veloso <dveloso@acessocard.com.br>
2016-04-20 09:08:06 -0300

    Parallel

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -126,6 +126,49 @@ namespace Threads
         //}
 
 
+        /*
+         Local Variavel
+         */
+
+        //public static ThreadLocal<int> _field =
+        //                                new ThreadLocal<int>(() =>
+        //                                {
+        //                                    return Thread.CurrentThread.ManagedThreadId;
+        //                                });
+
+        //public static void Main()
+        //{
+        //    new Thread(() =>
+        //    {
+        //        for (int x = 0; x < _field.Value; x++)
+        //        {
+        //            Console.WriteLine("Thread A: {0}", x);
+        //        }
+        //    }).Start();
+
+
+
+
+        //    new Thread(() =>
+        //    {
+        //        for (int x = 0; x < _field.Value; x++)
+        //        {
+        //            Console.WriteLine("Thread B: {0}", x);
+        //        }
+        //    }).Start();
+
+
+        //    Console.ReadKey();
+        //}
+
+        public static void Main()
+        {
+            ThreadPool.QueueUserWorkItem((s) =>
+            {
+                Console.WriteLine("Working on a thread from threadpool");
+            });
+            Console.ReadLine();
+        }
 
     }
 }
