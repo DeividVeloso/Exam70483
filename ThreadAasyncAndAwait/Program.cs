5eebc8fe4cbcb004fc6d2ca46df93d7133796325
SoulReaperNZ <veloso.deivid@gmail.com>
2016-04-24 19:31:06 -0300

    Novos

---
+++ ThreadAasyncAndAwait/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadAasyncAndAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = DownloadContent().Result;
+            Console.WriteLine("Sexo Antes");
+
+            Console.WriteLine(result);
+        }
+
+        public static async Task<string> DownloadContent()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                Thread.Sleep(300000);
+                string result = await client.GetStringAsync("http://www.microsoft.com");
+                return result;
+            }
+        }
+    }
+
+
+}
