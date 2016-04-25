00b062587b9eef48865f68740ea264ca13a4771d
Deivid Higino Veloso <dveloso@acessocard.com.br>
2016-04-25 12:24:40 -0300

    Novo

--- AsParallel/Program.cs
+++ AsParallel/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AsParallel
@@ -11,12 +12,21 @@ namespace AsParallel
         static void Main(string[] args)
         {
             var numbers = Enumerable.Range(0,100);
+            
             var parallelResult = numbers.AsParallel().Where(i => i % 2 == 0).ToArray();
 
             for (int i = 0; i < parallelResult.Length; i++)
             {
                 Console.WriteLine(parallelResult[i].ToString());
             }
+
+            /*
+             Parallel runtime defene se vai rodar em paralelo ou sincrono, para forçar a execução em paralelo usar o método WithExecutionMode()
+             * Você também pode limitar quantos processos o paralelismo irá usar com WithDegreeOfParallelism(), normalmente o PLINQ usa todos os processadores.
+             * Com o parelelismo não é possivel vir em order, pois depende dos processadores livres para processar o resultado, porém é possível usar o AsOrdered()
+             * para ordenar o resultado do parelelismo
+             * Para garantir que o código rode em sequencia usar AsSequential()
+             */
         }
     }
 }
---
+++ ForAll/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(0, 100);
+            var parallelResult = numbers.AsParallel().WithDegreeOfParallelism(6)
+            .Where(i => i % 2 == 0);
+
+            parallelResult.ForAll(e => Console.WriteLine(e));
+
+            /*
+             Usando o for no Parallel 
+             */
+        }
+    }
+}
