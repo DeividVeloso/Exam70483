b10b4089596ae2020a78290f919be49555da0c84
Deivid Higino Veloso <dveloso@acessocard.com.br>
2016-04-25 17:06:55 -0300

    Novo

---
+++ ConcurrentDictionaryUsing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrentDictionaryUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new ConcurrentDictionary<string, int>();
+            if (dict.TryAdd("k1", 42))
+            {
+                Console.WriteLine("Added");
+            }
+            if (dict.TryUpdate("k1", 21, 42))
+            {
+                Console.WriteLine("42 updated to 21");
+            }
+            dict["k1"] = 42; // Overwrite unconditionally
+            Console.WriteLine(dict["k1"]);
+            int r1 = dict.AddOrUpdate("k1", 3, (s, i) => i * 2);
+            Console.WriteLine(r1);
+            int r2 = dict.GetOrAdd("k2", 3);
+            Console.WriteLine(r2);
+        }
+    }
+}
