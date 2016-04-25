ad593dd1707b39a42db0c4c8fcef27fdaa594026
SoulReaperNZ <veloso.deivid@gmail.com>
2016-04-25 08:10:53 -0300

    Novos

---
+++ AsParallel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsParallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(0,100);
+            var parallelResult = numbers.AsParallel().Where(i => i % 2 == 0).ToArray();
+
+            for (int i = 0; i < parallelResult.Length; i++)
+            {
+                Console.WriteLine(parallelResult[i].ToString());
+            }
+        }
+    }
+}
