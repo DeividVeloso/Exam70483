651af88938c211dc2cf3b0b76dfce1911edf1799
Deivid Higino Veloso <dveloso@acessocard.com.br>
2016-04-25 14:12:30 -0300

    New

--- AggregateExceptionUsing/Program.cs
+++ AggregateExceptionUsing/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AggregateExceptionUsing
@@ -10,6 +11,27 @@ namespace AggregateExceptionUsing
     {
         static void Main(string[] args)
         {
+            var numbers = Enumerable.Range(0, 20);
+            try
+            {
+                var parallelResult = numbers.AsParallel()
+                .Where(i => IsEven(i));
+                parallelResult.ForAll(e => Console.WriteLine(e));
+
+               // Console.ReadLine();
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine("There where {0} exceptions",
+                e.InnerExceptions.Count);
+            }
+        }
+
+        public static bool IsEven(int i)
+        {
+            Thread.Sleep(1000);
+            if (i % 10 == 0) throw new ArgumentException("i");
+            return i % 2 == 0;
         }
     }
 }
