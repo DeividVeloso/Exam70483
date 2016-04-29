716b411fe645fcd90b25137bcb9bb7afd1507220
SoulReaperNZ <veloso.deivid@gmail.com>
2016-04-28 21:12:02 -0300

    NullCoalescing

---
+++ NullCoalescingOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullCoalescingOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? x = null;
+            int? z = 2;
+            int y = x ?? z ?? -1;
+            /*
+             Verifica se x é diferente de null se for coloca valor de x
+             Senão z e se z for null coloca o próximo valor
+             */
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+        }
+    }
+}
