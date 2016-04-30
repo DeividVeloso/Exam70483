c60415b6a7386d333dc896201c22443b1c9b08cc
Deivid Higino Veloso <dveloso@acessocard.com.br>
2016-04-30 01:53:37 -0300

    Lambda

---
+++ LambdaExpressionDelegate/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressionDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.UseDelegate();
+        }
+
+        public delegate int Calculate(int x, int y);
+
+        public void UseDelegate()
+        {
+            Calculate calc = (x, y) => x + y;
+            Console.WriteLine(calc(3, 4)); // Displays 7
+
+            calc = (x, y) => x * y;
+            Console.WriteLine(calc(3, 4)); // Displays 12
+        }
+    }
+}
