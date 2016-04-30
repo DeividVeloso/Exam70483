49d098e39f2ffd4e8a2fc588fa4682bdbaff2a4d
Deivid Higino Veloso <dveloso@acessocard.com.br>
2016-04-29 23:01:05 -0300

    Delegate

---
+++ 08-ContinueWithCanceledException/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _08_ContinueWithCanceledException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancellationTokenSource.Token;
+            Task task = Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Console.Write("*");
+                    Thread.Sleep(1000);
+                }
+            }, token).ContinueWith((t) =>
+            {
+                t.Exception.Handle((e) => true);
+                Console.WriteLine("You have canceled the task");
+            }, TaskContinuationOptions.OnlyOnCanceled);
+
+            Task longRunning = Task.Run(() =>
+                                {
+                                    Thread.Sleep(10000);
+                                });
+            int index = Task.WaitAny(new[] { longRunning }, 1000);
+            if (index == -1)
+                Console.WriteLine("Task timed out");
+            //Console.WriteLine("Press enter to stop the task");
+            //Console.ReadLine();
+
+            //cancellationTokenSource.Cancel();
+
+            //Console.WriteLine("Press enter to end the application");
+            //Console.ReadLine();
+        }
+    }
+}
