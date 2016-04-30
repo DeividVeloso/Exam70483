08027d96217cdf4d560190028669d650c64c46f9
Deivid Higino Veloso <dveloso@acessocard.com.br>
2016-04-30 00:28:25 -0300

    Delegate

---
+++ MulticastDelegateUsing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticastDelegateUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.Multicast();
+        }
+
+        public void MethodOne()
+        {
+            Console.WriteLine("MethodOne");
+        }
+        public void MethodTwo()
+        {
+            Console.WriteLine("MethodTwo");
+        }
+        public delegate void Del();
+        public void Multicast()
+        {
+            Del d = MethodOne;
+            d += MethodTwo;
+            d();
+        }
+
+    }
+}
