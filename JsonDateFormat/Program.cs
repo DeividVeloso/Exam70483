23efdcd6a7e260b810cf6e1bdea46cedc5a7a74c
Deivid Higino Veloso <dveloso@acessocard.com.br>
2016-05-02 14:32:21 -0300

    JsonDateSerializado

---
+++ JsonDateFormat/Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonDateFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.WriteJsonDates();
+        }
+
+        public class LogEntry
+        {
+            public string Details { get; set; }
+            public DateTime LogDate { get; set; }
+        }
+
+        public void WriteJsonDates()
+        {
+            LogEntry entry = new LogEntry
+            {
+                LogDate = new DateTime(2009, 2, 15, 0, 0, 0, DateTimeKind.Utc),
+                Details = "Application started."
+            };
+
+            // default as of Json.NET 4.5
+            string isoJson = JsonConvert.SerializeObject(entry);
+            // {"Details":"Application started.","LogDate":"2009-02-15T00:00:00Z"}
+
+            JsonSerializerSettings microsoftDateFormatSettings = new JsonSerializerSettings
+            {
+                DateFormatHandling = DateFormatHandling.MicrosoftDateFormat
+            };
+            string microsoftJson = JsonConvert.SerializeObject(entry, microsoftDateFormatSettings);
+            // {"Details":"Application started.","LogDate":"\/Date(1234656000000)\/"}
+
+            string javascriptJson = JsonConvert.SerializeObject(entry, new JavaScriptDateTimeConverter());
+            // {"Details":"Application started.","LogDate":new Date(1234656000000)}
+        }
+    }
+}
---
+++ JsonDateFormat/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JsonDateFormat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JsonDateFormat")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("72c3d725-4be4-4563-bdd4-eb1432c7f16e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
