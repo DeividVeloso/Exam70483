931a483f045a193d6441b8359f7003c2d399074f
Deivid Higino Veloso <dveloso@acessocard.com.br>
2016-05-04 17:51:52 -0300

    1

---
+++ ExceptionDispatchInfoThrow/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDispatchInfoThrow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExceptionDispatchInfo possibleException = null;
+            try
+            {
+                string s = Console.ReadLine();
+                int.Parse(s);
+            }
+            catch (FormatException ex)
+            {
+                possibleException = ExceptionDispatchInfo.Capture(ex);
+            }
+            if (possibleException != null)
+            {
+                possibleException.Throw();
+            }
+
+            /*
+                Você pode usar exibir thrown exception do erro em outra classe e mostra junto com outro exception
+             * 
+             */
+        }
+    }
+}
