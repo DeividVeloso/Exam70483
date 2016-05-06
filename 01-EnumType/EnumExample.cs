33ebbf14f47ace9bb1c1db1020b0ea300e14927a
Deivid Higino Veloso <dveloso@acessocard.com.br>
2016-05-06 15:19:18 -0300

    ValueTypes

---
+++ 01-EnumType/EnumExample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class EnumExample
+{
+
+    //public enum Days { Seg = 1, Ter, Quar, Quin, Sex, Sab, Dom};
+
+    [Flags]
+    public enum Days
+    {
+        None = 0x0,
+        Sunday = 0x1,
+        Monday = 0x2,
+        Tuesday = 0x4,
+        Wednesday = 0x8,
+        Thursday = 0x10,
+        Friday = 0x20,
+        Saturday = 0x40
+    }
+   // Days readingDays = Days.Monday | Days.Saturday;
+}
---
+++ 01-EnumType/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_EnumType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var obj = new EnumExample();
+
+
+            switch (EnumExample.Days.Friday)
+            {
+                case EnumExample.Days.None:
+                    break;
+                case EnumExample.Days.Sunday:
+                    break;
+                case EnumExample.Days.Monday:
+                    break;
+                case EnumExample.Days.Tuesday:
+                    break;
+                case EnumExample.Days.Wednesday:
+                    break;
+                case EnumExample.Days.Thursday:
+                    break;
+                case EnumExample.Days.Friday:
+                    Console.WriteLine(EnumExample.Days.Friday);
+                    break;
+                case EnumExample.Days.Saturday:
+                    break;
+                default:
+                    break;
+            }      
+        }
+    }
+}
