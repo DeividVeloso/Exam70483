695d7691ee842607c18371d9b6a6914369120758
SoulReaperNZ <veloso.deivid@gmail.com>
2016-05-06 19:59:37 -0300

    Dynamic

---
+++ CastingUsing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastingUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Base class input in a derived class, use casting explicit
+            Object stream = new MemoryStream();
+            MemoryStream memoryStream = (MemoryStream)stream;
+
+            //Derived class input in Base class, don't use casting, because is implicit
+            MemoryStream stream2 = new MemoryStream();
+            Object implicitObj = stream2;
+
+
+
+        }
+
+
+    }
+}
