7fee254cd6a8cd1be1acd478fb819a132c062c34
Deivid Higino Veloso <dveloso@acessocard.com.br>
2016-05-10 14:08:09 -0300

    novos

---
+++ EnumExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public static class EnumExample
+{
+    public static EnumExample()
+    {
+    }
+
+    //public enum Days { Seg = 1, Ter, Quar, Quin, Sex, Sab, Dom};
+
+    [Flags]
+    public static enum Days
+    {
+        None = 0x0,
+        Sunday = 0x1,
+        Monday = 0x2,
+        Tuesday = 0x4,
+        Wednesday = 0x8,
+        Thursday = 0x10,
+        Friday = 0x20,
+        Saturday = 0x40
+    }
+    Days readingDays = Days.Monday | Days.Saturda;
+}
---
+++ FuncWithLambdaAnonymousMethods/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncWithLambdaAnonymousMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> addFunc = (x, y) => x + y;
+            Func<int, int, int> subFunc = (x, y) => x - y;
+            Console.WriteLine(addFunc(2, 3));
+            Console.WriteLine(subFunc(2, 3));
+
+        }
+    }
+}
