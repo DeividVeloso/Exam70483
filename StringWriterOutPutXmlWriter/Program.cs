af011f9024919f13657d6c54cdbf80c1e9ee0e74
Deivid Higino Veloso <dveloso@acessocard.com.br>
2016-05-12 13:21:14 -0300

    XML

---
+++ StringWriterOutPutXmlWriter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace StringWriterOutPutXmlWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Gera o XML*/
+            var stringWriter = new StringWriter();
+            using (XmlWriter writer = XmlWriter.Create(stringWriter))
+            {
+                writer.WriteStartElement("book");
+                writer.WriteElementString("price", "19.95");
+                writer.WriteEndElement();
+                writer.Flush();
+            }
+            string xml = stringWriter.ToString();
+
+            /*Lê o XML e transforma em  dados*/
+            var stringReader = new StringReader(xml);
+
+            using (XmlReader reader = XmlReader.Create(stringReader))
+            {
+                reader.ReadToFollowing("price");
+                decimal price = decimal.Parse(reader.ReadInnerXml(),
+                new CultureInfo("en-US")); // Make sure that you read the decimal part correctly
+            }
+        }
+
+    }
+}
