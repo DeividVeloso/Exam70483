5b241eab3690d226d379d1cbeb18bfc8d1606d9f
Deivid Higino Veloso <dveloso@acessocard.com.br>
2016-05-19 14:45:06 -0300

    Debugg

---
+++ DebbugerUsing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebbugerUsing
+{
+    using System;
+    using System.Threading;
+    public static class Program
+    {
+        public static void Main()
+        {
+            Timer t = new Timer(TimerCallback, null, 0, 2000);
+            Console.ReadLine();
+        }
+        private static void TimerCallback(Object o)
+        {
+            Console.WriteLine("In TimerCallback: " + DateTime.Now);
+            GC.Collect();
+        }
+    }
+}
