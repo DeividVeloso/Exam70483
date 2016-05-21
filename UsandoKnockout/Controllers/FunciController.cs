54911e42896c0188965d73690ad82a1e654e66d4
DeividVeloso <veloso.deivid@gmail.com>
2016-05-21 11:12:21 -0300

    KNO

---
+++ UsandoKnockout/Controllers/FunciController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UsandoKnockout.Controllers
+{
+    public class FunciController : Controller
+    {
+        // GET: Funci
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+    }
+}
---
+++ UsandoKnockout/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UsandoKnockout.Startup))]
+namespace UsandoKnockout
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
