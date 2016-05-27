3a09ef3aa18c1c50e95f4ae776c4761a03adb3dd
Deivid Veloso <veloso.deivid@gmail.com>
2016-05-27 13:51:33 -0300

    proejto js

---
+++ calcula-imc.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+
+var peso = 73;
+var peso = 1.85;
+
+var imc = peso / (altura * altura)
+
+console.log(imc);
---
+++ intro.js
@@ -0,0 +1,4 @@
+// JavaScript source code
+//var nome = prompt("Digite o seu nome aqui: ");
+console.log("Oi");
+console.log("Tchau");
