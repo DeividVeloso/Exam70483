ea4beb37ce76f2894c970a3c228868d8f052009f
Deivid Higino Veloso <dveloso@acessocard.com.br>
2016-06-02 16:15:11 -0300

    teste

--- Task/Program.cs
+++ Task/Program.cs
@@ -23,3 +23,71 @@ namespace TaskExample
         }
     }
 }
+  //              var emailResquest = new Acesso.BusinessObjects.Mail.Modelo.MailRequest();
+  //              emailResquest.Formato = BusinessObjects.Mail.Enums.MailFormato.Html;
+  //              emailResquest.SenderAddress = request.Data.email;
+  //              emailResquest.Subject = assuntoMensagem;
+  //              emailResquest.MailBody = conteudoMensagem.ToString();
+  //              emailResquest.ToAdress = ConfigurationManager.AppSettings["FaleConoscoDestinatario"];
+
+  //              var emailResult = MailWs.Enviar(emailResquest); 
+                
+  //              if(emailResult.Status == BusinessObjects.Mail.Enums.MailStatus.Erro)
+  //                  throw new ServiceException(1, emailResult.Mensagem);
+
+
+
+  //          var mailRequest = new MailRequest
+  //          {
+  //              Formato = BusinessObjects.Mail.Enums.MailFormato.Html,
+  //              ReplyAddress = "naoresponda@acessocard.com.br",
+  //              SenderAddress = "naoresponda@acessocard.com.br",
+  //              Subject = usuario.PrimeiroNome + " lhe convidou para adquirir o cartão Acesso pré-pago",
+  //              NameSender = usuario.NomeCompleto
+  //          };
+  //          mailRequest.MailBody = _CriarMensagemIndicacaoEmail(message, usuario);
+  //          foreach (var item in emails)
+  //          {
+  //              mailRequest.ToAdress = item;
+  //              var mailres = MailWs.Enviar(mailRequest);
+  //              if (mailres.Status != BusinessObjects.Mail.Enums.MailStatus.Sucesso)
+  //                  LogUtils.Add("GestaoMGM.EnviarEmailIndicacao", mailres.Mensagem);
+  //          }
+
+
+  //                          var mailRequest = new MailRequest
+  //                          {
+  //                              ReplyAddress = item.ReplyAddress,
+  //                              NameSender = item.NameSender,
+  //                              SenderAddress = item.SenderAddress,
+  //                              ToAdress = item.Email,
+  //                              Subject = item.Subject,
+  //                              MailBody = GetHtmlEmail(item.Url).
+  //                                  Replace("##NOME##", item.Nome).
+  //                                  Replace("##EMAIL##", item.Email).
+  //                                  Replace("##LINKBOLETO##", item.LinkBoleto).
+  //                                  Replace("##DATAVENCTO##", item.DataVencto.ToShortDateString()).
+  //                                  Replace("##LINK2VIABOLETO##", item.Link2viaBoleto).
+  //                                  Replace("##DD/MM/AAAA", item.DtCriacao.ToShortDateString()).
+  //                                  Replace("##HH:MM", item.DtCriacao.ToShortTimeString())
+  //                          };
+
+  //                          var mailResult = mail.Enviar(mailRequest);
+  //                          if (mailResult.Status == MailStatus.Erro)
+  //                          {
+  //                              Trace.TraceError("Erro ao processar eventos primários! {0}", mailResult.Mensagem);
+  //                              LogUtils.Add("Regua.ProcessamentoPrimario", mailResult.Mensagem);
+  //                          }
+
+
+  //private static string GetHtmlEmail(string path)
+  //      {
+  //          string html;
+
+  //          using (var reader = new StreamReader(path, Encoding.UTF8))
+  //          {
+  //              html = reader.ReadToEnd();
+  //          }
+
+  //          return html;
+  //      }
