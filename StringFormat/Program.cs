7798943b74f8085b4fcf3f6ca53f2c14a7799af3
Deivid Higino Veloso <dveloso@acessocard.com.br>
2016-07-04 18:11:52 -0300

    StringFormat

---
+++ StringFormat/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double number = 150.33333;
+
+            string MyString = number.ToString("0.##;-0.##");
+            string MyString2 = number.ToString("+#;-#;0");
+
+   
+            Console.WriteLine(MyString);
+            Console.WriteLine(MyString2);
+        }
+    }
+}
